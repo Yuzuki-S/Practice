8e1caa7eefeb8c1d7238583fe3ff6de5aa39e3f6
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-09-24 10:42:38 +1200

    reached to db

--- server/server.js
+++ server/server.js
@@ -6,4 +6,17 @@ var server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
+
+server.get('/add', (req, res) => {
+    console.log('hit')
+    let package = req.query;
+    db.savePackage(package, req.user.username)
+    .then(
+      data => {
+       res.json(data); 
+      }
+    )
+   })
+
+
 module.exports = server
